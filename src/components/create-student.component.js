82937d4c7cf3f34eb135f4775ab0c5c3f15ffb49
yasher <yasher@businessgateways.com>
2020-03-04 20:34:10 +0530

    2nd commit

---
+++ src/components/create-student.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+export default class CreateStudent extends Component {
+    render() {
+        return (
+            <div>
+                <p>
+                    create student component
+                </p>
+            </div>
+        );
+
+    }
+}
